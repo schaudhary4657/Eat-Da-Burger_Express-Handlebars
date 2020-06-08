985f67359c0de9173e450de1b4f195bc5aaf298e
Sapana Chaudhary <sapana.chy23@gmail.com>
2020-06-07 23:44:39 -0700

    working code, fixed the devour it button

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Tofu and Bacon", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Tofu and smoked goat", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Tofu and goat cheese", false);
+
+SELECT * from burgers;
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(methodOverride("_method"));
 
 // Set Handlebars
 const exphbs = require("express-handlebars");
